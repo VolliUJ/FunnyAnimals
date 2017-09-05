bddd13eb3bb50db701fb13e847d28e53f94e77d9
MGOLLI <mgolli@MacBook-Pro-MCZYZ.local>
2017-09-06 01:03:15 +0200

    Initial Commit

---
+++ FunnyAnimals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunnyAnimals
+//
+//  Created by MGOLLI on 06.09.2017.
+//  Copyright © 2017 MGOLLI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
